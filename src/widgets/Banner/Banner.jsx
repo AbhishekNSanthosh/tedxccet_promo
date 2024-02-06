003f3b5213ca95fcbfc7bbebd5a87dd93b8a71cd
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2024-02-06 19:01:32 +0530

    feat:aos update

--- src/widgets/Banner/Banner.jsx
+++ src/widgets/Banner/Banner.jsx
@@ -1,22 +1,15 @@
-"use client"
-
-import React, { useEffect } from 'react'
+import React from 'react'
 import styles from '../../themes/banner.module.css'
-import Aos from 'aos';
-import 'aos/dist/aos.css'
 
 const Banner = () => {
-  useEffect(() => {
-    Aos.init({ duration: 800 });
-  }, [])
   return (
     <div className={styles.container}>
-      <img src="../logo.png" alt="" className={styles.banner} data-aos="fade-up"/>
-      <div className={styles.row} data-aos="fade-up">
+      <img src="../logo.png" alt="" className={styles.banner} />
+      <div className={styles.row}>
         <span className={styles.comingsoon}>COMINGx</span>
         <span className={styles.comingsoonRed}>SOON</span>
       </div>
-      <div className={styles.row} data-aos="fade-up">
+      <div className={styles.row}>
         <img src="/staytuned.svg" alt="" className={styles.staytuned} />
       </div>
     </div>
