6b73ba3d533bb66c86e11efe742ebcbcc6b54f94
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2024-02-14 18:51:19 +0530

    feat:SEO added

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,6 +14,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link rel="icon" type="image/png+xml" href="/tedx.png" />
+        <meta name="keywords"
+          content="tedxccet,tedx,Tedx ccet,tedx ccet,carmel tedx,tedx carmel,carmelcet,carmel college of engineering, carmel punnapra,tedx carmel,carmel tedx , sparkz ccet, sparkz ccet ,Carmel CET, Carmel college, Carmel college tedx,tedx event carmel,Tedx ,Tedx kerala,tedx 2024 ,tedx ,TEDX,Carmel College of Engineering and Technology,ccet alappuzha,tedx&nbsp;ccet&nbsp;Alappuzha"></meta>
         <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,200;1,800&display=swap" rel="stylesheet"></link>
       </head>
       <body className={inter.className}>{children}</body>
