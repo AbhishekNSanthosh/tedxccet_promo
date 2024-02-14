c114669f616c793d8d5b6a3ef38c6854d2f01920
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2024-02-14 18:41:58 +0530

    fix:Banner image resolution updated

--- src/widgets/Banner/Banner.jsx
+++ src/widgets/Banner/Banner.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import styles from '../../themes/banner.module.css'
+import Image from 'next/image'
 
 const Banner = () => {
   return (
     <div className={styles.container}>
-      <img src="../logo.png" alt="" className={styles.banner} />
+      <Image src="/banner.svg" width={600} height={150} className={styles.banner}/>
       <div className={styles.row}>
         <span className={styles.comingsoon}>COMINGx</span>
         <span className={styles.comingsoonRed}>SOON</span>
       </div>
       <div className={styles.row}>
-        <img src="/staytuned.svg" alt="" className={styles.staytuned} />
+      <Image src="/staytuned.svg" width={300} height={0} className={styles.staytuned}/>
       </div>
     </div>
   )
