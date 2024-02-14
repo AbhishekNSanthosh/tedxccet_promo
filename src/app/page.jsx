9c709c3f95321316df052dc2807e2de58a27c2ee
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2024-02-14 19:19:59 +0530

    feat:splash screen test version added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,23 +1,48 @@
+"use client"
+
+import React, { useState, useEffect } from 'react';
 import Banner from '@/widgets/Banner/Banner'
 import Navbar from '@/widgets/Navbar/Navbar'
-import React from 'react'
 import styles from './page.module.css'
 import About from '@/widgets/About/About'
 import Footer from '@/widgets/Footer/Footer'
+import Image from 'next/image'
 
 const page = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate an asynchronous operation (e.g., fetching data) during initial loading
+    const fetchData = async () => {
+      // Simulate an API call or any asynchronous operation
+      await new Promise((resolve) => setTimeout(resolve, 6000));
+
+      // Set loading to false once the data is loaded
+      setLoading(false);
+    };
+
+    fetchData();
+  }, []);
   return (
-    <div className={styles.container}>
-      <Navbar />
-      <Banner />
-      <section id='about'>
-        <About />
-      </section>
-      {/* <hr /> */}
-      <section id='contact'>
-        <Footer />
-      </section>
-    </div>
+    <>
+      {loading ?
+        <div className={styles.splashContainer}>
+          <img src="/splash.gif" className={styles.splash} />
+        </div>
+        :
+        <div className={styles.container}>
+          <Navbar />
+          <Banner />
+          <section id='about'>
+            <About />
+          </section>
+          {/* <hr /> */}
+          <section id='contact'>
+            <Footer />
+          </section>
+        </div>
+      }
+    </>
   )
 }
 
